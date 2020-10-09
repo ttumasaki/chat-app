d55c3327e2a62ce2c3d0a4e57ff71226982945be
ttumasaki <be_da_masa@ybb.ne.jp>
2020-10-09 16:09:48 +0900

    チャットルーム作成機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "messages#index"
-  resources:users, only: [:edit, :update]
-  resources:rooms, only: [:new, :create]
+  resources :users, only: [:edit, :update]
+  resources :rooms, only: [:new, :create]
 end
